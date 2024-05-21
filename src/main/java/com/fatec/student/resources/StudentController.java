fbee99e1ad1f205eec472ff12e6e605fa729fb04
Gustavo Ferreira <gfmb94@gmail.com>
2024-05-21 11:24:51 -0300

    Insert the controllers gets, delete and save

--- src/main/java/com/fatec/student/resources/StudentController.java
+++ src/main/java/com/fatec/student/resources/StudentController.java
@@ -7,8 +7,15 @@ import com.fatec.student.services.StudentService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 @RequestMapping("students")
@@ -21,4 +28,17 @@ public class StudentController {
     public List<Student> getStudents(){
         return studentService.getStudents();
     }
+    @GetMapping("{id}")
+    public Student getStudentById(@PathVariable int id){
+        return studentService.getStudentById(id);
+    }
+    @DeleteMapping("{id}")
+    public void deleteStudentById(@PathVariable int id){
+        this.studentService.deleteStudentById(id);
+    }
+    @PostMapping
+    public Student save(@RequestBody Student student){
+        return this.studentService.save(student);
+    }
+    
 } 
